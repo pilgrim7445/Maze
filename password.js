00258d3e847575c320fbef8f5d164199a28df1fd
Pilgrim <152723591+pilgrim7445@users.noreply.github.com>
2025-06-11 09:29:05 +0100

    Update password.js

--- password.js
+++ password.js
@@ -1,8 +1,8 @@
 alert("This is my maze.");
 var pw = prompt("Password: ");
-if (pw === 0807) {
-  alert("correct")
+if (pw === '0807') {  // Use string for password comparison
+  alert("correct");
 } else {
-  alert("incorrect")
-  window.location.replace("blank.page");
+  alert("incorrect");
+  window.location.replace("blank.page");  // Ensure the URL is correct and accessible
 }
