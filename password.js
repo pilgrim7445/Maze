6ae3d3eb888b30affc6d925c9731617fddc86996
Pilgrim <p34215166@gmail.com>
2026-02-09 12:26:17 +0000

    Change password prompt variable to let and update redirect

--- password.js
+++ password.js
@@ -1,8 +1,7 @@
-alert("This is my maze.");
-var pw = prompt("Password: ");
+let pw = prompt("Password: ");
 if (pw === '0807') {
   alert("Correct");
 } else {
   alert("Incorrect");
-  window.location.replace("404.html");
+  window.location.replace("about:blank");
 }
